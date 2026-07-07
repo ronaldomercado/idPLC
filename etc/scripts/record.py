93dcb8971a473a118bd5f687423bc4967e53ce27
Ronaldo Mercado <ronaldo.mercado@diamond.ac.uk>
2026-07-07 14:08:43 +0100

    capture script to record potentiometer position

---
+++ etc/scripts/record.py
@@ -0,0 +1,69 @@
+#!/dls_sw/prod/python3/RHEL7-x86_64/softioc/4.4.0/prefix/bin/pythonSoftIOC
+#
+# script to record pot positions
+#
+# set up pots
+import sys
+import cothread
+import epicscorelibs.path.cothread
+from cothread.catools import caget, camonitor, caput
+from cothread.cothread import Sleep
+
+def potClear(axis):
+    caput(f"TS02K-MO-AXIS-0{axis}:PS:CLEAR.PROC",1)
+
+def program(index, axis, pot, enc):
+    indexPv=f"TS02K-MO-AXIS-0{axis}:PS:INDEX"
+    caput(indexPv,index)
+    potvaluePV=f"TS02K-MO-AXIS-0{axis}:PS:OUT:POT"
+    encvalueOutPV=f"TS02K-MO-AXIS-0{axis}:PS:OUT:ENC"
+    caput(potvaluePV, pot)
+    caput(encvalueOutPV, enc)
+    Sleep(0.5)
+    caput(f"TS02K-MO-AXIS-0{axis}:PS:SETIDX.PROC", 1)
+    Sleep(0.5)
+
+def moveToMain(axis):
+    caput(f"TS02K-MO-AXIS-0{axis}:PS:END.PROC",1)
+
+
+output=open("data-scan-2026-05-13-500um.txt","w")
+demand = "TS02K-MO-SERVC-01:CS:GAP"
+start_gap = 4
+step=.5
+end_gap = 100
+gap = start_gap
+index = 0
+
+for axis in range(1,5):
+    potClear(axis)
+
+okay=True
+while okay:
+    print(f"caput for gap {gap}")
+    caput(demand, gap, wait=True, timeout=10)
+    print("caput done")
+    Sleep(2)
+    for axis in range(1,5):
+        encoderPv = f"TS02K-MO-SERVO-0{axis}:MOT.RBV"
+        rawPv = f"TS02K-MO-AXIS-0{axis}:RAW"
+        pot=caget(rawPv)
+        enc=caget(encoderPv)
+        print(gap, axis, pot, enc, file=output)
+        print(gap, axis, pot, enc, file=sys.stdout)
+        program(index,axis, pot, enc)
+    print(f"programming index {index} done", file=sys.stdout)
+    gap = gap + step
+    index += 1
+    if step > 0:
+        if gap > end_gap:
+            okay = False
+    else:
+        if gap < end_gap:
+            okay = False
+
+for axis in range(1,5):
+    moveToMain(axis)
+
+
+output.close()
