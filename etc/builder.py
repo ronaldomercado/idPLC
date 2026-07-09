e6a03bb048638819f6ea0d6653179e7fb25e08e4
Ronaldo Mercado <ronaldo.mercado@diamond.ac.uk>
2026-07-09 15:54:15 +0100

    add idplc_status template

--- etc/builder.py
+++ etc/builder.py
@@ -2,22 +2,23 @@ from iocbuilder import AutoSubstitution, ModuleBase, records, Device, Xml
 
 # templates related to ID interface
 class NX102_tilt(AutoSubstitution):
-    TemplateFile = "NX102_tilt.template"
+    TemplateFile = 'NX102_tilt.template'
 
 class NX102_tiltpot(AutoSubstitution):
-    TemplateFile = "NX102_tiltpot.template"
+    TemplateFile = 'NX102_tiltpot.template'
 
 class NX102_axisexp(AutoSubstitution):
-    TemplateFile = "NX102_axisexp.template"
+    TemplateFile = 'NX102_axisexp.template'
 
 class NX102_id(AutoSubstitution):
-    TemplateFile = "NX102_id.template"
+    TemplateFile = 'NX102_id.template'
 
 class NX102_idoverrides(AutoSubstitution):
-    TemplateFile = "NX102_idoverrides.template"
+    TemplateFile = 'NX102_idoverrides.template'
 
 class NX102_idpotscan(AutoSubstitution):
-    TemplateFile = "NX102_idpotscan.template"
+    TemplateFile = 'NX102_idpotscan.template'
     
-
+class Status(AutoSubstitution):
+    TemplateFile = 'idplc_status.template'
 
